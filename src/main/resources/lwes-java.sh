74dd5f61e59df70f541e4ce9e07b52650c823d8d
Frank Maritato <frank.maritato@openx.com>
2012-09-18 11:42:15 -0700

    export variables and modify PATH

--- src/main/resources/lwes-java.sh
+++ src/main/resources/lwes-java.sh
@@ -1,2 +1,4 @@
-LWES_JAVA_HOME=${lwes.java.home}
-LWES_JAVA_VERSION=${project.version}
+export LWES_JAVA_HOME=${lwes.java.home}
+export LWES_JAVA_VERSION=${project.version}
+
+export PATH=$LWES_JAVA_HOME/bin:$PATH
