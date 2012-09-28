affd378412bd9441cb5f3365d37dc56cda47b0c3
Frank Maritato <frank.maritato@openx.com>
2012-09-28 14:13:15 -0700

    license changes

--- src/main/java/org/lwes/emitter/BroadcastEmitterGroup.java
+++ src/main/java/org/lwes/emitter/BroadcastEmitterGroup.java
@@ -1,4 +1,14 @@
-// Copyright OpenX Limited 2010. All Rights Reserved.
+/*======================================================================*
+ * Copyright OpenX Limited 2010. All Rights Reserved.                   *
+ *                                                                      *
+ * Licensed under the New BSD License (the "License"); you may not use  *
+ * this file except in compliance with the License.  Unless required    *
+ * by applicable law or agreed to in writing, software distributed      *
+ * under the License is distributed on an "AS IS" BASIS, WITHOUT        *
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.     *
+ * See the License for the specific language governing permissions and  *
+ * limitations under the License. See accompanying LICENSE file.        *
+ *======================================================================*/
 package org.lwes.emitter;
 
 import java.io.IOException;
@@ -9,7 +19,7 @@ import org.lwes.Event;
 /**
  * This class emits an event to all members of the group.
  * 
- * @author Joel Meyer (joel.meyer@openx.org)
+ * @author Joel Meyer
  */
 public class BroadcastEmitterGroup extends EmitterGroup {
   private static final Logger LOG = Logger.getLogger(BroadcastEmitterGroup.class);
--- src/main/java/org/lwes/emitter/MOfNEmitterGroup.java
+++ src/main/java/org/lwes/emitter/MOfNEmitterGroup.java
@@ -1,4 +1,14 @@
-// Copyright OpenX Limited 2010. All Rights Reserved.
+/*======================================================================*
+ * Copyright OpenX Limited 2010. All Rights Reserved.                   *
+ *                                                                      *
+ * Licensed under the New BSD License (the "License"); you may not use  *
+ * this file except in compliance with the License.  Unless required    *
+ * by applicable law or agreed to in writing, software distributed      *
+ * under the License is distributed on an "AS IS" BASIS, WITHOUT        *
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.     *
+ * See the License for the specific language governing permissions and  *
+ * limitations under the License. See accompanying LICENSE file.        *
+ *======================================================================*/
 package org.lwes.emitter;
 
 import java.io.IOException;
@@ -14,7 +24,7 @@ import org.lwes.Event;
  * {@link AtomicInteger} is retrieved and incremented and the values of
  * [index + 0, ... , index + M] mod N are used to select the emitters.
  * 
- * @author Joel Meyer (joel.meyer@openx.org)
+ * @author Joel Meyer
  */
 public class MOfNEmitterGroup extends BroadcastEmitterGroup {
   private static final Logger LOG = Logger.getLogger(MOfNEmitterGroup.class);
@@ -34,7 +44,7 @@ public class MOfNEmitterGroup extends BroadcastEmitterGroup {
   }
 
   /**
-   * @see org.openx.lwes.emittergroup.BroadcastEmitterGroup#emitToGroup(org.lwes.Event)
+   * @see org.lwes.emitter.BroadcastEmitterGroup#emitToGroup(org.lwes.Event)
    */
   @Override
   protected void emit(Event e) {
