lwes/lwes-java

commit c4b12ae3e8d6369f88177dd62a7f3781532f59ab

bug fix for array event to handle nullable arrays

src/main/java/org/lwes/ArrayEvent.java
@@ -23,12 +23,16 @@ import java.util.SortedSet;
 import java.util.TreeSet;
 
 import org.apache.commons.lang3.mutable.MutableInt;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.lwes.serializer.Deserializer;
 import org.lwes.serializer.DeserializerState;
 import org.lwes.serializer.Serializer;
 import org.lwes.util.EncodedString;
 
 public final class ArrayEvent extends DefaultEvent {
+
+    private static transient final Log log = LogFactory.getLog(ArrayEvent.class);
 
     private static final int SERIALIZED_ENCODING_LENGTH;
     private byte[] bytes = new byte[MAX_MESSAGE_SIZE];
@@ -362,6 +366,16 @@ public final class ArrayEvent extends DefaultEvent {
                 return Deserializer.deserializeFloatArray(state, bytes);
             case IP_ADDR_ARRAY:
                 return Deserializer.deserializeIPADDRArray(state, bytes);
+            case NLONG_ARRAY:
+                return Deserializer.deserializeNLongArray(state, bytes);
+            case NDOUBLE_ARRAY:
+                return Deserializer.deserializeNDoubleArray(state, bytes);
+            case NFLOAT_ARRAY:
+                return Deserializer.deserializeNFloatArray(state, bytes);
+            case NINTEGER_ARRAY:
+                return Deserializer.deserializeNIntegerArray(state, bytes);
+            case NSHORT_ARRAY:
+                return Deserializer.deserializeNShortArray(state, bytes);
         }
         throw new IllegalStateException("Bad type: " + type);
     }
@@ -435,14 +449,15 @@ public final class ArrayEvent extends DefaultEvent {
                 }
                 else {
                     // Wrong field.  Skip it, the type token, and the value.
-                    tempState.incr(1 + keyLength);
+                    tempState.incr(1 + keyLength); // field name
                     final FieldType type = FieldType.byToken(bytes[tempState.currentIndex()]);
-                    tempState.incr(1);
-                    tempState.incr(getValueByteSize(type, tempState.currentIndex()));
+                    tempState.incr(1); // type token
+                    Deserializer.deserializeValue(tempState, bytes, type, (short) 1);
                 }
             }
             if (tempState.currentIndex() > length) {
-                throw new IllegalStateException("Overran the end of the byte array");
+                throw new IllegalStateException(
+                        "Overran the end of the byte array: " + tempState.currentIndex() + " " + length);
             }
             return -1;
         }
@@ -480,10 +495,12 @@ public final class ArrayEvent extends DefaultEvent {
             case FLOAT:
             case IPADDR:
             case NINTEGER:
+            case NFLOAT:
                 return 4;
             case INT64:
             case UINT64:
             case DOUBLE:
+            case NDOUBLE:
             case NLONG:
                 return 8;
             case STRING:
@@ -512,6 +529,15 @@ public final class ArrayEvent extends DefaultEvent {
             case UINT64_ARRAY:
             case DOUBLE_ARRAY:
                 return 2 + deserializeUINT16(valueIndex) * 8;
+            case NLONG_ARRAY:
+            case NDOUBLE_ARRAY:
+                // length + (num items * 8 bytes) + (bitset length * 2 bytes)
+                return 2 + (deserializeUINT16(valueIndex) * 8) + (deserializeUINT16(valueIndex) * 2);
+            case NINTEGER_ARRAY:
+            case NFLOAT_ARRAY:
+                return 2 + (deserializeUINT16(valueIndex) * 4) + (deserializeUINT16(valueIndex) * 2);
+            case NSHORT_ARRAY:
+                return 2 + (deserializeUINT16(valueIndex) * 2) + (deserializeUINT16(valueIndex) * 2);
         }
         throw new IllegalStateException("Unrecognized type: " + type);
     }

src/main/java/org/lwes/FieldType.java
@@ -107,6 +107,12 @@ public enum FieldType {
         return name;
     }
 
+    public boolean isNullableArray() {
+        return (this == NSHORT_ARRAY || this == NDOUBLE_ARRAY || this == NFLOAT_ARRAY ||
+                this == NBIGINT_ARRAY || this == NBOOLEAN_ARRAY || this == NBYTE_ARRAY ||
+                this == NINTEGER_ARRAY || this == NLONG_ARRAY);
+    }
+
     public boolean isArray() {
         return array;
     }

src/main/java/org/lwes/serializer/Deserializer.java
@@ -494,13 +494,18 @@ public class Deserializer {
     public static BitSet deserializeBitSet(DeserializerState myState, byte[] bytes) {
 
         int size = deserializeINT16(myState, bytes);
+        if (log.isDebugEnabled()) {
+            log.debug("BitSet length: "+size);
+        }
         BitSet bitSet = new BitSet(size);
         int offset = myState.currentIndex();
         for (int i = offset; i < (offset + size); i++) {
             bitSet.set((i - offset), bytes[i] == 1);
             myState.incr(1);
         }
-
+        if (log.isDebugEnabled()) {
+            log.debug("BitSet: "+bitSet);
+        }
         return bitSet;
     }
 

src/main/java/org/lwes/serializer/Serializer.java
@@ -493,6 +493,10 @@ public class Serializer {
     }
 
     public static int serializeBitSet(BitSet bitSet, byte[] data, int offset) {
+        if (log.isDebugEnabled()) {
+            log.debug("BitSet: " + bitSet);
+        }
+
         int offsetStart = offset;
         offset += serializeINT16((short) bitSet.length(), data, offset);
         for (int i = 0; i < bitSet.length(); i++) {
@@ -538,7 +542,7 @@ public class Serializer {
         offset += serializeBitSet(bitSet, bytes, offset);
         // Now write the float values
         System.arraycopy(tmp, 0, bytes, offset, tmpOffset);
-        offset += tmp.length;
+        offset += tmpOffset;
 
         return (offset - offsetStart);
     }
@@ -571,7 +575,7 @@ public class Serializer {
         offset += serializeBitSet(bitSet, bytes, offset);
         // Now write the float values
         System.arraycopy(tmp, 0, bytes, offset, tmpOffset);
-        offset += tmp.length;
+        offset += tmpOffset;
 
         return (offset - offsetStart);
     }
@@ -604,9 +608,10 @@ public class Serializer {
         offset += serializeBitSet(bitSet, bytes, offset);
         // Now write the float values
         System.arraycopy(tmp, 0, bytes, offset, tmpOffset);
-        offset += tmp.length;
+        offset += tmpOffset;
 
-        return (offset - offsetStart);    }
+        return (offset - offsetStart);
+    }
 
     public static int serializeNLongArray(Long[] data, byte[] bytes, int offset) {
         int numbytes = 0;
@@ -635,10 +640,12 @@ public class Serializer {
         // Write the bitset first to ease with deserialization
         offset += serializeBitSet(bitSet, bytes, offset);
         // Now write the float values
+        log.debug("write " + tmpOffset + " bytes");
         System.arraycopy(tmp, 0, bytes, offset, tmpOffset);
-        offset += tmp.length;
+        offset += tmpOffset;
 
-        return (offset - offsetStart);    }
+        return (offset - offsetStart);
+    }
 
     public static int serializeNShortArray(Short[] data, byte[] bytes, int offset) {
         int numbytes = 0;
@@ -668,9 +675,10 @@ public class Serializer {
         offset += serializeBitSet(bitSet, bytes, offset);
         // Now write the float values
         System.arraycopy(tmp, 0, bytes, offset, tmpOffset);
-        offset += tmp.length;
+        offset += tmpOffset;
 
-        return (offset - offsetStart);    }
+        return (offset - offsetStart);
+    }
 
     public static int serializeNIntegerArray(Integer[] data, byte[] bytes, int offset) {
         int numbytes = 0;
@@ -700,7 +708,8 @@ public class Serializer {
         offset += serializeBitSet(bitSet, bytes, offset);
         // Now write the float values
         System.arraycopy(tmp, 0, bytes, offset, tmpOffset);
-        offset += tmp.length;
+        offset += tmpOffset;
 
-        return (offset - offsetStart);    }
+        return (offset - offsetStart);
+    }
 }

src/test/java/org/lwes/ArrayEventTest.java
@@ -9,19 +9,19 @@
  *======================================================================*/
 package org.lwes;
 
-import junit.framework.Assert;
-import org.apache.log4j.Logger;
-import org.junit.Test;
-
 import java.math.BigInteger;
 import java.util.Arrays;
 import java.util.Enumeration;
+
+import org.apache.log4j.Logger;
+import org.junit.Test;
+
+import junit.framework.Assert;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
 public final class ArrayEventTest extends EventTest {
 
@@ -179,6 +179,36 @@ public final class ArrayEventTest extends EventTest {
     public void testInvalidEncodingType() throws EventSystemException {
         final ArrayEvent event = new ArrayEvent("Event");
         event.set("enc", FieldType.INT32, Event.DEFAULT_ENCODING);
+    }
+
+    @Test
+    public void testNullableEvents() {
+        Event evt = new ArrayEvent("Event");
+        evt.set("long[]", FieldType.NLONG_ARRAY, new Long[] { 5000000000l, null, 8675309l });
+        Long[] retrievedArray = evt.getLongObjArray("long[]");
+        for (Long l : retrievedArray) {
+            log.debug("retrieved item: "+l);
+        }
+        evt.set("str", FieldType.STRING, "testing");
+        Assert.assertEquals(5000000000l, retrievedArray[0].longValue());
+        Assert.assertNull(evt.getLongObjArray("long[]")[1]);
+        Assert.assertEquals("testing", evt.getString("str"));
+
+        evt.set("double[]", FieldType.NDOUBLE_ARRAY, new Double[] { 1.23, 1.26, null });
+        Assert.assertEquals(1.23, evt.getDoubleObjArray("double[]")[0]);
+        Assert.assertNull(evt.getDoubleObjArray("double[]")[2]);
+
+        evt.set("float[]", FieldType.NFLOAT_ARRAY, new Float[] { 1.11f, null, 1.12f });
+        Assert.assertEquals(1.11f, evt.getFloatObjArray("float[]")[0]);
+        Assert.assertNull(evt.getFloatObjArray("float[]")[1]);
+
+        evt.set("integer[]", FieldType.NINTEGER_ARRAY, new Integer[] { 5000, null, 12345 });
+        Assert.assertEquals(5000, evt.getIntegerObjArray("integer[]")[0].intValue());
+        Assert.assertNull(evt.getIntegerObjArray("integer[]")[1]);
+
+        evt.set("short[]", FieldType.NSHORT_ARRAY, new Short[] { 5, null, 10 });
+        Assert.assertEquals(5, evt.getShortObjArray("short[]")[0].shortValue());
+        Assert.assertNull(evt.getShortObjArray("short[]")[1]);
     }
 
     @Override

src/test/java/org/lwes/FieldTypeTest.java
@@ -1,0 +1,28 @@
+package org.lwes;
+/**
+ * User: frank.maritato
+ * Date: 2/27/13
+ */
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+public class FieldTypeTest {
+
+    private static transient Log log = LogFactory.getLog(FieldTypeTest.class);
+
+    @Test
+    public void testIsNullableArray() {
+        Assert.assertTrue(FieldType.NLONG_ARRAY.isNullableArray());
+        Assert.assertFalse(FieldType.UINT64_ARRAY.isNullableArray());
+    }
+
+    @Test
+    public void testIsArray() {
+        Assert.assertTrue(FieldType.UINT64_ARRAY.isArray());
+        Assert.assertFalse(FieldType.UINT64.isArray());
+    }
+}

src/test/java/org/lwes/MapEventTest.java
@@ -4,16 +4,45 @@ package org.lwes;
  * Date: 5/2/12
  */
 
-import junit.framework.Assert;
+import java.math.BigInteger;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.junit.Test;
 
-import java.math.BigInteger;
+import junit.framework.Assert;
 
 public class MapEventTest extends EventTest {
 
     private static transient Log log = LogFactory.getLog(MapEventTest.class);
+
+    @Test
+    public void testNullableArrays() {
+        Event evt = new MapEvent("Event");
+
+        evt.set("long[]", FieldType.NLONG_ARRAY, new Long[] { 5000000000l, null, 8675309l });
+        evt.set("str", FieldType.STRING, "testing");
+        Long[] retrievedArray = evt.getLongObjArray("long[]");
+        Assert.assertEquals(5000000000l, retrievedArray[0].longValue());
+        Assert.assertNull(evt.getLongObjArray("long[]")[1]);
+        Assert.assertEquals("testing", evt.getString("str"));
+
+        evt.set("double[]", FieldType.NDOUBLE_ARRAY, new Double[] { 1.23, null, 1.26 });
+        Assert.assertEquals(1.23, evt.getDoubleObjArray("double[]")[0]);
+        Assert.assertNull(evt.getDoubleObjArray("double[]")[1]);
+
+        evt.set("float[]", FieldType.NFLOAT_ARRAY, new Float[] { 1.11f, 1.12f, null });
+        Assert.assertEquals(1.11f, evt.getFloatObjArray("float[]")[0]);
+        Assert.assertNull(evt.getFloatObjArray("float[]")[2]);
+
+        evt.set("integer[]", FieldType.NINTEGER_ARRAY, new Integer[] { 5000, null, 12345 });
+        Assert.assertEquals(5000, evt.getIntegerObjArray("integer[]")[0].intValue());
+        Assert.assertNull(evt.getIntegerObjArray("integer[]")[1]);
+
+        evt.set("short[]", FieldType.NSHORT_ARRAY, new Short[] { 5, null, 10 });
+        Assert.assertEquals(5, evt.getShortObjArray("short[]")[0].shortValue());
+        Assert.assertNull(evt.getShortObjArray("short[]")[1]);
+    }
 
     @Test
     public void testGettersSetters() {

src/test/java/org/lwes/serializer/SerializerTest.java
@@ -49,6 +49,21 @@ public class SerializerTest {
         Assert.assertTrue(dsbs.get(1));
         Assert.assertTrue(dsbs.get(2));
         Assert.assertFalse(dsbs.get(3));
+
+        bitSet = new BitSet(5);
+        bitSet.set(0);
+        bitSet.set(2);
+
+        ds = new DeserializerState();
+        offset = 0;
+        bytes = new byte[25];
+        Serializer.serializeBitSet(bitSet, bytes, offset);
+        dsbs = Deserializer.deserializeBitSet(ds, bytes);
+        Assert.assertEquals(2, dsbs.cardinality());
+        Assert.assertTrue(dsbs.get(0));
+        Assert.assertFalse(dsbs.get(1));
+        Assert.assertTrue(dsbs.get(2));
+        Assert.assertFalse(dsbs.get(3));
     }
 
     @Test
@@ -58,8 +73,8 @@ public class SerializerTest {
         };
 
         byte[] bytes = new byte[64];
-        Serializer.serializeValue(FieldType.NFLOAT_ARRAY, array, (short) 1, bytes, 0);
-
+        int num = Serializer.serializeValue(FieldType.NFLOAT_ARRAY, array, (short) 1, bytes, 0);
+        Assert.assertEquals(24, num);
         DeserializerState state = new DeserializerState();
         Float[] rtn = (Float[]) Deserializer.deserializeValue(state, bytes, FieldType.NFLOAT_ARRAY, (short) 1);
         Assert.assertNotNull(rtn);

commit 518b5369c97ba6c5e662ac500a39c9299ffd8ca1

Speed: Don't deserialize values in find(); just skip them.  66% speed boost. (fixed speed regression in c4b12ae3e8d6369f88177dd62a7f3781532f59ab)

src/main/java/org/lwes/ArrayEvent.java
@@ -390,7 +390,8 @@ public final class ArrayEvent extends DefaultEvent {
                     tempState.incr(1 + keyLength); // field name
                     final FieldType type = FieldType.byToken(bytes[tempState.currentIndex()]);
                     tempState.incr(1); // type token
-                    Deserializer.deserializeValue(tempState, bytes, type, (short) 1);
+                    // Skip the value without deserializing it
+                    tempState.incr(getValueByteSize(type, tempState.currentIndex()));
                 }
             }
             if (tempState.currentIndex() > length) {

src/test/java/org/lwes/ArrayEventPerformanceTest.java
@@ -30,7 +30,7 @@ public class ArrayEventPerformanceTest {
   
   // Change these values as performance shifts. If ArrayEvent gets faster, lower
   // them.  If we are forced to accept it getting slower, lower them.
-  private static final double DIRECT_GET_CPU_TIME = 76000;
+  private static final double DIRECT_GET_CPU_TIME = 26000;
   
   @BeforeClass
   public static void beforeClass() {