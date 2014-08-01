aec39c5d7e922ad75feb97426d323e4b901b3ebb
Julian Pellico <jp283u@yp.com>
2014-08-01 15:10:46 -0700

    Modify FilterListener to allow multiple values for an attribute (to form a whitelist)

--- src/main/java/org/lwes/listener/FilterListener.java
+++ src/main/java/org/lwes/listener/FilterListener.java
@@ -10,12 +10,16 @@
 package org.lwes.listener;
 /**
  * This is a filtering multicast listener. It can filter by event name or any attribute of an event.
+ * Specifying an attribute name more than once with multiple values creates a whitelist.
  *
  * Show only Test::Event events
  * java org.lwes.listener.FilterListener -m 224.0.0.1 -p 6001 -e "Test::Event"
  *
  * Show only events with eid=1234 AND aid=someapp
  * java org.lwes.listener.FilterListener -m 224.0.0.1 -p 6001 -a "eid=1234,aid=someapp"
+ *
+ * Show only events with aid=someapp OR aid=anotherapp
+ * java org.lwes.listener.FilterListener -m 224.0.0.1 -p 6001 -a "eid=1234,aid=someapp,aid=anotherapp"
  *
  * Events are printed to stdout one per line.
  *
@@ -33,11 +37,7 @@ import org.lwes.EventSystemException;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class FilterListener implements EventHandler {
 
@@ -59,7 +59,7 @@ public class FilterListener implements EventHandler {
     private String attrList = null;
 
     private List<String> eventNames;
-    private Map<String, String> eventAttrs;
+    private Map<String, Set<String>> eventAttrs;
 
     public void processArguments(String[] args) {
         CmdLineParser parser = new CmdLineParser(this);
@@ -72,12 +72,19 @@ public class FilterListener implements EventHandler {
             throw new RuntimeException(e.getMessage(), e);
         }
         if (attrList != null && !attrList.isEmpty()) {
-            eventAttrs = new HashMap<String, String>();
+            eventAttrs = new HashMap<String, Set<String>>();
             String[] kvList = attrList.split(",");
             for (String kv : kvList) {
                 String[] pairs = kv.trim().split("=");
                 if (pairs.length == 2) {
-                    eventAttrs.put(pairs[0].trim(), pairs[1].trim());
+                    if (eventAttrs.containsKey(pairs[0].trim())) {
+                        eventAttrs.get(pairs[0].trim()).add(pairs[1].trim());
+                    }
+                    else {
+                        HashSet<String> hashSet = new HashSet<String>();
+                        hashSet.add(pairs[1].trim());
+                        eventAttrs.put(pairs[0].trim(), hashSet);
+                    }
                 }
                 else {
                     log.warn(kv + " not a valid k=v pair");
@@ -122,12 +129,25 @@ public class FilterListener implements EventHandler {
         new FilterListener().run(args);
     }
 
-    public Map<String, String> getEventAttrs() {
+    public Map<String, Set<String>> getEventAttrs() {
         return eventAttrs;
     }
 
-    public void setEventAttrs(Map<String, String> eventAttrs) {
-        this.eventAttrs = eventAttrs;
+    /**
+     * You can still use setEventAttrs the old way by passing an int as the second parameter,
+     * that way you would not have to rewrite your code if you were using this method
+     */
+    public void setEventAttrs(Map<String, String> eventAttrs, int ignored) {
+        this.eventAttrs = new HashMap<String, Set<String>>();
+        for (String key : eventAttrs.keySet()) {
+            Set<String> valSet = new HashSet<String>(1);
+            valSet.add(eventAttrs.get(key));
+            this.eventAttrs.put(key, valSet);
+        }
+    }
+
+    public void setEventAttrs(Map<String, Set<String>> eventAttrSets) {
+        eventAttrs = eventAttrSets;
     }
 
     public List<String> getEventNames() {
@@ -154,10 +174,10 @@ public class FilterListener implements EventHandler {
         if (eventAttrs != null) {
             for (String key : eventAttrs.keySet()) {
                 Object o = event.get(key);
-                String val = eventAttrs.get(key);
+                Set<String> vals = eventAttrs.get(key);
                 // If more than one kv pair is submitted, events must meet
                 // all conditions. If any of them fail, return.
-                if (!(o != null && val.equals(o.toString()))) {
+                if (!(o != null && vals.contains(o.toString()))) {
                     return null;
                 }
             }
